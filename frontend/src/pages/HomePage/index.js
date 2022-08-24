665d922c9f37e60ec540b56c54675faa0de41922
André de Souza <andreoew@hotmail.com>
2022-08-24 15:45:15 -0300

    create input and button in page home

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -1,18 +1,42 @@
 import React from 'react';
-import { Container } from'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Header from '../../components/Header';
+import { Container, InputGroup, FormControl, Button, Alert } from 'react-bootstrap';
+import { ContentContainer, Form } from './styles'
 
 class HomePage extends React.Component {
+  
   constructor(props) {
     super(props);
+
+    this.state = {
+      isLoanding: false,
+      url: '',
+      shortenedURL: '',
+      errorMessage: '',
+    }    
   }
+  
 
   render() {
+    
     return (
       <Container>
         <Header>Seu novo encurtador de URL. :)</Header>
-        <FontAwesomeIcon icon="check-square" />
+        <ContentContainer>
+          <Form>
+            <InputGroup>
+              <FormControl 
+                placeholder="Digite a url para encurtar"
+                defaultValue=""
+                onChange={e => this.setState({ url: e.target.value})}
+              />              
+                <Button 
+                  variant="primary" type="submit">Encurtar
+                </Button>            
+              
+            </InputGroup>
+          </Form>
+        </ContentContainer>        
       </Container>
     )
   }
