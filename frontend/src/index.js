51b159afd5aeaf30b8b52a839f12d5c2052df86d
André de Souza <andreoew@hotmail.com>
2022-08-24 14:40:36 -0300

    create hearde and include icon

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.scss';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCheckSquare);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
