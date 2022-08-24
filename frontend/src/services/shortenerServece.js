4f66ccc99227c4f649dc34add086ce0d8e1c534c
André de Souza <andreoew@hotmail.com>
2022-08-24 00:29:05 -0300

    create shortenerService

--- frontend/src/services/shortenerServece.js
+++ frontend/src/services/shortenerServece.js
@@ -4,4 +4,22 @@ class ShortenerService {
   constructor() {
     this.api = baseAPI('http://localhost:3001/');
   }
+
+  async getLink(code) {
+    const result = await this.api.get(`links/${code}`);
+
+    return result.data;
+  }
+
+  async getStats(code) {
+    const result = await this.api.get(`links/${code}/stats`);
+
+    return result.data;
+  }
+
+  async generate(model) {
+    const result = await this.api.post('links', url)
+  }
 }
+
+export default ShortenerService;
